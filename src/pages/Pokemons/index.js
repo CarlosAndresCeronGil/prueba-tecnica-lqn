ae31d3712c37864ddd07b9d91739c7cd0d0ee999
CarlosAndresCeronGil <andrescg0106@hotmail.com>
2022-04-10 08:14:16 -0500

    Second exercise
    Finished second exercise

--- src/pages/Pokemons/index.js
+++ src/pages/Pokemons/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export default function Pokemons() {
     const [finalArray, setFinalArray] = useState([])
@@ -76,21 +76,62 @@ export default function Pokemons() {
     "yamask",
   ];
 
-  useEffect(function() {
-    const auxCount = 0
-    const auxArray = []
-    for(var i = 0; i <= POKEMON_LIST.length; i++){
+  // const LISTA_PERSONAL = [
+  //   'orochi',
+  //   'asuma',
+  //   'luz',
+  //   'alma',
+  //   'isol',
+  //   'sona'
+  // ]
 
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    const auxArray = []
+    const lenghtAux = []
+    var auxCount = 0
+    var nuevaIteracion = true
+    for(var i = 0; i < POKEMON_LIST.length; i++){
+        if(nuevaIteracion===true){
+          auxArray.splice(0, auxArray.length)
+          auxArray.push(POKEMON_LIST[auxCount])
+          nuevaIteracion = false
+        }
+        if((auxArray[auxArray.length - 1] !== POKEMON_LIST[i]) && (auxArray[auxArray.length - 1].slice(-1) === POKEMON_LIST[i].slice(0,1)) && (auxArray.some(pokemon => pokemon === POKEMON_LIST[i]) === false) ) {
+          auxArray.push(POKEMON_LIST[i])
+          i = -1
+        }
+        if(i+1 === POKEMON_LIST.length){
+          lenghtAux.push(auxArray.length)
+          if(lenghtAux.length > 1) {
+            // if(lenghtAux[lenghtAux.length - 2] < auxArray.length) {
+            if(lenghtAux.every(n => n < auxArray.length)) {
+              setFinalArray(auxArray)
+            }
+          }else {
+            setFinalArray(auxArray)
+          }
+          auxCount++
+          i = -1
+          if(auxCount < POKEMON_LIST.length){
+            nuevaIteracion = true
+          }else {
+            break
+          }
+        }
     }
-  }, [])
+  }
 
   return <div>
-      {
-          POKEMON_LIST.map(singlePokemon => (
-              <p key={singlePokemon}>
-                  {singlePokemon}
-              </p>
-          ))
-      }
+      <form onSubmit={handleSubmit}>
+        {
+            finalArray.map(singlePokemon => (
+                <p key={singlePokemon}>
+                    {singlePokemon}
+                </p>
+            ))
+        }
+      <button>Ver lista</button>
+      </form>
   </div>;
 }
