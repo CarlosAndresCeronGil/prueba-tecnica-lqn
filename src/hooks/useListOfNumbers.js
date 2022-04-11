291abefca6b599328eb2600472c20e55ef1aaf54
CarlosAndresCeronGil <andrescg0106@hotmail.com>
2022-04-11 09:01:30 -0500

    Correction
    Correction on useListOfNumbers

--- src/hooks/useListOfNumbers.js
+++ src/hooks/useListOfNumbers.js
@@ -5,13 +5,15 @@ export function useListOfNumbers() {
 
   useEffect(function () {
     const aux = [];
-    for (var i = 0; i <= 100; i++) {
-      if (i % 2 === 0) {
+    for (var i = 1; i <= 100; i++) {
+      if (i % 2 === 0 && i % 5 === 0) {
+        aux.push(i + " buzz bazz");
+      } else if (i % 2 === 0) {
         aux.push(i + " buzz");
-      } else if (i % 5 === 0) {
-        aux.push(i + " bazz");
-      } else {
-        aux.push(i);
+      } else if(i % 5 === 0){
+        aux.push(i + "bazz");
+      }else {
+        aux.push(i)
       }
     }
     setNumbers(aux);
